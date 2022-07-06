d1b0407a9cbeeca098df69a389f053baa3518e3c
jvlinkin <jvlimapaiva@gmail.com>
2022-07-06 16:24:55 -0300

    configsss heroku

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 8081
 
+
+
+
 //Json as response:
 app.use(express.json())
 //Connection DataBase:
@@ -41,6 +44,4 @@ app.use('/login', loginRoute)
 const showUsers = require('./routes/showUsers')
 app.use('/showusers', showUsers)
 
-app.listen(8081, () =>{
-    console.log(`Listening on ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
